3fdfece0b2349c70a92dd7243fefe8c12642ba03
Oksana <oksanavasilevna77@gmail.com>
2025-01-14 00:12:34 +0200

    add service and moviedb-api.js

--- src/service/moviedb-api.js
+++ src/service/moviedb-api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 export const API_KEY = "8770b58debb691d0e718d7b9db131187";
+// JWT - Javascript Web Token
 export const API_JWT =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzcwYjU4ZGViYjY5MWQwZTcxOGQ3YjlkYjEzMTE4NyIsIm5iZiI6MTczNjQ0MTc5NS4yNjMsInN1YiI6IjY3N2ZmZmMzNjAxYWNmZTdiZDRlOWYyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0psSbOjbE3w7G1qN-NwmiMvIe9JcTnLUR9949RBFgl8";
-export const BASE_URL = "https://api.themoviedb.org";
+export const BASE_URL = "https://api.themoviedb.org/3";
 export const search = "/search/movie";
 export const headers = {
-  Authorization: `Bearer ${API_JWT}`,
+  Authorization: `Bearer ${API_JWT}`, //Bearer - type authorization
 };
 
 export const fetchMovies = async (url = search, { query } = {}) => {
@@ -14,6 +15,7 @@ export const fetchMovies = async (url = search, { query } = {}) => {
     params: { query },
     baseURL: BASE_URL,
     headers: headers,
+    
   });
 
   return data;
