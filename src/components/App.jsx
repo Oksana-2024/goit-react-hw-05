bb4693ab64da0a0170bb624d8bbcae8308107410
Oksana <oksanavasilevna77@gmail.com>
2025-01-14 00:12:49 +0200

    edit app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,34 @@
 import { Route, Routes } from "react-router-dom";
+import Navigation from "./Navigation/Navigation";
+import MovieCast from "./MovieCast/MovieCast";
+import MovieReviews from "./MovieReviews/MovieReviews";
+import { lazy, Suspense } from "react";
 import "./App.css";
-import HomePage from "./pages/HomePage/HomePage";
-import MoviesPage from "./pages/MoviesPage/MoviesPage";
-import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
+import { ToastContainer } from "react-toastify";
+
+const pages = {
+  Home: lazy(() => import("./pages/HomePage/HomePage")),
+  Movies: lazy(() => import("./pages/MoviesPage/MoviesPage")),
+  MovieDetails: lazy(() => import("./pages/MovieDetailsPage/MovieDetailsPage")),
+  NotFound: lazy(() => import("./pages/NotFoundPage/NotFoundPage")),
+};
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/MoviesPage" element={<MoviesPage />} />
-        <Route path="/MovieDetailsPage" element={<MovieDetailsPage />} />
-      </Routes>
+      <Navigation />
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<pages.Home />} />
+          <Route path="/Movies" element={<pages.Movies />} />
+          <Route path="/movies/:movieId" element={<pages.MovieDetails />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<pages.NotFound />} />
+        </Routes>
+      </Suspense>
+      <ToastContainer />
     </>
   );
 }
