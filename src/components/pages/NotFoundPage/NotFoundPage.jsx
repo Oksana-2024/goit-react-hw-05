e069a6f80cd9094516c71abf7266c2034e8d6140
Oksana <oksanavasilevna77@gmail.com>
2025-01-14 00:11:42 +0200

    add page not found

--- src/components/pages/NotFoundPage/NotFoundPage.jsx
+++ src/components/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,4 +1,6 @@
+import s from "./NotFoundPage.module.css";
+
 const NotFoundPage = () => {
-  return <div></div>;
+  return <div className={s.text}><h2>Page not found</h2></div>;
 };
 export default NotFoundPage;
