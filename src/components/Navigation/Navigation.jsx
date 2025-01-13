595212342a4e058e9af9e8778a21cc09bff69587
Oksana <oksanavasilevna77@gmail.com>
2025-01-14 00:10:14 +0200

    add component Navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom";
+import s from "./Navigation.module.css";
+
+const Navigation = () => {
+  const linkActive = ({ isActive }) => (isActive ? s.active : s.link);
+  return (
+    <nav className={s.nav} >
+      <ul className={s.list}>
+        <li>
+          <NavLink to="./" className={linkActive}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="./Movies" className={linkActive}>Movies</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navigation;
