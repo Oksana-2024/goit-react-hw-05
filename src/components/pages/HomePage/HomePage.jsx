b48cb1b7efa94b255c139bc665d5eba30756339c
Oksana <oksanavasilevna77@gmail.com>
2025-01-14 00:10:38 +0200

    add Home page

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import MovieList from "../../MovieList/MovieList";
+import { fetchMovies } from "../../../service/moviedb-api";
+import s from "./HomePage.module.css"
+
 const HomePage = () => {
-  return <div></div>;
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetchMovies("/trending/movie/day")
+      .then((response) => {
+        setMovies(response.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally();
+  }, []);
+  return (
+    <div className={s.container}>
+     <h2 className={s.title}> Trending today</h2>
+      <MovieList movies={movies} />
+    </div>
+  );
 };
 export default HomePage;
