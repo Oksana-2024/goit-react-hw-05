0cc5bcdf10ff085702107330315d8fcdc92b328a
Oksana <oksanavasilevna77@gmail.com>
2025-01-14 00:09:21 +0200

    add component MovieCast

---
+++ src/components/MovieCast/MovieCast.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { fetchMovies } from "../../service/moviedb-api";
+import { useParams } from "react-router-dom";
+import s from "./MovieCast.module.css"
+
+const MovieCast = () => {
+  const { movieId } = useParams();
+  const [cast, setCast] = useState(null);
+  const defaultImg =
+    "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
+  useEffect(() => {
+    fetchMovies(`/movie/${movieId}/credits`)
+      .then((response) => {
+        setCast(response.cast);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally();
+  }, []);
+
+  return (
+    <ul className={s.listCast}>
+      {cast?.length > 0 && (
+        cast.map(({ name, character, id, profile_path }) => {
+          return (
+            <li className={s.cardCast} key={id}>
+              <img
+                src={
+                  profile_path
+                    ? `https://image.tmdb.org/t/p/w500/${profile_path}`
+                    : defaultImg
+                }
+                alt={name}
+                width={120}
+              />
+            <div className={s.wrap}>
+                <h4>{name}</h4>
+               <div className={s.character}>
+                  <h4>Character:</h4>
+                  <p>{character}</p>
+               </div>
+            </div>
+            </li>
+          );
+        })
+      )} 
+      { cast && !cast.length && (
+        <li>
+          <p> We don&apos;t have any cast for this movie</p>
+        </li>
+      )}
+    </ul>
+  );
+};
+export default MovieCast;
